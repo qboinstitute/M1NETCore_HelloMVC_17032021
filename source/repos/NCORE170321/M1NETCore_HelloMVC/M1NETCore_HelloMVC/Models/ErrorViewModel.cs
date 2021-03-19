48a758e0c0966a85977018713d80ce755054ed8a
Luis Chang <luis.chang@outlook.com>
2021-03-19 12:45:09 -0500

    Commit message

---
+++ source/repos/NCORE170321/M1NETCore_HelloMVC/M1NETCore_HelloMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace M1NETCore_HelloMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
